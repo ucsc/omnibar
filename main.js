24dbadb17051481346d339daeb7411ae7da2a88f
Lee <leewhite128@gmail.com>
2016-09-22 11:59:19 -0700

    began making mobile friendly

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 function subButton(txt) {
-  window.location.href = "http://www.google.com/search?q=" + txt + " site:ucsc.edu";
+  var url = "http://www.google.com/search?q=" + txt + " site:ucsc.edu";
+  window.open(url);
 }
 
 function checkSubmit() {
    if(e && e.keyCode == 13) {
       document.forms[0].submit();
    }
+}
+
+function expand() {
+	var nav = document.getElementById("menuItems");
+	if (nav.className === "menuNav") {
+		nav.className += " responsive";
+	} else {
+		nav.className = "menuNav"
+	}
 }
